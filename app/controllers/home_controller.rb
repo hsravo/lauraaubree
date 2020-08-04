88f30073b1644d99bf2c4dd4eca048d3e07487fb
Steph Ravo <hsravo1@gmail.com>
2020-08-04 20:13:41 +0200

    dashboard graphics edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
   def index
   end
 
+  def dashboard
+    @projects = Project.all
+    @publications = Publication.all
+  end
+
   def admin
     if user_signed_in?
       if current_user.role == 'admin'
